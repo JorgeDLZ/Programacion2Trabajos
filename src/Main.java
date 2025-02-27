3193ef6fe3c312959f68459fd39903bec9d57e44
JorgeDLZ <jorgediazlozano11@gmail.com>
2025-02-27 17:59:52 -0500

    Dados

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,41 @@
-/*  La diferencia entre el switch case viejo y el switch case nuevo
- de Java 14 radica principalmente en la sintaxis, funcionalidad y facilidad de uso.
- Ya que mientras en la version vieja, se precisa del uso de un break
- para poner terminar la ejecucion del codigo, en la version de java 14
- podemos hacerlo de una manera mucho más efeciente, así mismo logrando hacerlo
-  de una manera más compacta a la hora de programar
- */
-// Sintaxis Antigua (Java 13 y versiones anteriores)
-//La sintaxis tradicional usa la palabra clave case con break
-// para evitar la ejecución de múltiples casos:
-//Switch case viejo:
-public class SwitchVersion {
+import java.util.Random;
+public class Main {
     public static void main(String[] args) {
-        int day = 2;
 
-        switch (day) {
-            case 1:
-                System.out.println("Lunes");
-                break;
-            case 2:
-                System.out.println("Martes");
-                break;
-            case 3:
-                System.out.println("Miércoles");
-                break;
-            case 4:
-                System.out.println("Jueves");
-                break;
-            case 5:
-                System.out.println("Viernes");
-                break;
-            case 6:
-                System.out.println("Sábado");
-                break;
-            case 7:
-                System.out.println("Domingo");
-                break;
-            default:
-                System.out.println("Día no válido");
-                break;
+        int max = 10;
+        int min = 1;
+        int num;
+        int dado1;
+        int dado2;
+        int rondas;
+        int cont1=0,cont2=0, suma1=0,suma2=0;
+
+
+        Random ale = new Random();
+        rondas=ale.nextInt(max-min+1)+min;
+        System.out.println("Cantidad de rondas: " + rondas);
+
+
+
+        for (int i =1;i<=rondas;i++){
+            dado1 = ale.nextInt(6-1+1) + 1;
+            cont1 += dado1;
+
+
+            dado2= ale.nextInt(6-1+1) + 1;
+            cont2 += dado1;
+
+            System.out.println();
+            System.out.println("D1 "+ dado1);
+            System.out.println("D2 "+ dado2);
+
+            System.out.println();
+
+            if(cont1>cont2
+
+
+
         }
-    }
-}
-//Switch case nuevo
-public class SwitchVersion {
-    public static void main(String[] args) {
-        int day = 2;
 
-        String resultado = switch (day) {
-            case 1 -> "Lunes";
-            case 2 -> "Martes";
-            case 3 -> "Miércoles";
-            case 4 -> "Jueves";
-            case 5 -> "Viernes";
-            case 6 -> "Sábado";
-            case 7 -> "Domingo";
-            default -> "Día no válido";
-        };
-
-        System.out.println(resultado);
     }
 }
