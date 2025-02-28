7430f232a3c3f3998d78dc99eef6417d81d12e19
JorgeDLZ <jorgediazlozano11@gmail.com>
2025-02-27 21:14:07 -0500

    Dados

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,37 @@
 import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-
-        int max = 10;
-        int min = 1;
-        int num;
-        int dado1;
-        int dado2;
-        int rondas;
-        int cont1=0,cont2=0, suma1=0,suma2=0;
-
+        int max = 10, min = 1;
+        int rondas, dado1, dado2;
+        int cont1 = 0, cont2 = 0;
 
         Random ale = new Random();
-        rondas=ale.nextInt(max-min+1)+min;
+        rondas = ale.nextInt(max - min + 1) + min;
         System.out.println("Cantidad de rondas: " + rondas);
 
+        for (int i = 1; i <= rondas; i++) {
+            dado1 = ale.nextInt(6) + 1;
+            dado2 = ale.nextInt(6) + 1;
 
-
-        for (int i =1;i<=rondas;i++){
-            dado1 = ale.nextInt(6-1+1) + 1;
             cont1 += dado1;
+            cont2 += dado2;
 
-
-            dado2= ale.nextInt(6-1+1) + 1;
-            cont2 += dado1;
-
+            System.out.println("Ronda " + i + ":");
+            System.out.println("Dado 1: " + dado1);
+            System.out.println("Dado 2: " + dado2);
             System.out.println();
-            System.out.println("D1 "+ dado1);
-            System.out.println("D2 "+ dado2);
-
-            System.out.println();
-
-            if(cont1>cont2
-
-
-
         }
 
+        System.out.println("Suma total Dado 1: " + cont1);
+        System.out.println("Suma total Dado 2: " + cont2);
+
+        if (cont1 > cont2) {
+            System.out.println("Dado 1 gana");
+        } else if (cont2 > cont1) {
+            System.out.println("Dado 2 gana");
+        } else {
+            System.out.println("Es un empate");
+        }
     }
 }
